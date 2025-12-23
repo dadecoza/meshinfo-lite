4d834cc88b1978476006ec3b262042e030d71f0f
Johannes le Roux <dade@dade.co.za>
2025-12-23 21:50:19 +0200

    check for type key

--- meshdata.py
+++ meshdata.py
@@ -840,7 +840,7 @@ WHERE id = %s ORDER BY ts_created DESC LIMIT 1"""
             frm = data["from"]
             via = self.int_id(topic.split("/")[-1])
             self.verify_node(frm, via)
-        tp = data.get("type") or "unknown"
+        tp = data["type"] if "type" in data else "unknown"
         if tp == "nodeinfo":
             self.store_node(data)
         elif tp == "position":
